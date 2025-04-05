76868edb627ddf74db915bc07bb09329bf12b095
ciscooceguera <oceguera.14@hotmail.com>
2025-04-05 15:30:00 -0700

    Cambié todo lo hecho con JOptionPane a Scanner

--- Main.java
+++ Main.java
@@ -1,76 +1,61 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int opc = 0;
         while (opc!= 3) {
             // pregunto que acció quiere realizar el usuario
-            Object[] botones = {"Jugar","Reglas","Créditos","Salir"};
-            opc = JOptionPane.showOptionDialog(null,
-                    "Selecciona la opción",
-                    "El Mago de las Palabras",
-                    JOptionPane.DEFAULT_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    botones,
-                    botones[0]);
-            System.out.println(opc);
+            System.out.println("1. Jugar\n2. Reglas\n3. Créditos\n4. Salir\nIngresa una opción: ");
+            opc = sc.nextInt();
+            sc.nextLine();
             // switch de la opción seleccionada
             switch (opc){
                 // opción jugar
-                case 0:
-                    // solicito # jugadores
-                    Object[] botonesNumJugadores = {2, 3, 4};
-                    Integer numSeleccionado = (Integer) JOptionPane.showOptionDialog(null,
-                            "Ingresa número de jugadores",
-                            "Número de jugadores",
-                            JOptionPane.DEFAULT_OPTION,
-                            JOptionPane.QUESTION_MESSAGE,
-                            null,
-                            botonesNumJugadores,
-                            botonesNumJugadores[0]);
-                    // solicito modalidad
-                    Object[] botonesModalidades = {"Regular", "Experto"};
-                    int modalidadSeleccionada = (Integer) JOptionPane.showOptionDialog(null,
-                            "Ingresa el modo de juego",
-                            "Selección de dificultad",
-                            JOptionPane.DEFAULT_OPTION,
-                            JOptionPane.QUESTION_MESSAGE,
-                            null,
-                            botonesModalidades,
-                            botonesModalidades[0]);
+                case 1:
+                    int numJugadores = 0;
+                    while (numJugadores<2 || numJugadores>4){
+                        // solicito # jugadores
+                        System.out.println("\nIngresa el número de jugadores (2 - 4): ");
+                        numJugadores = sc.nextInt();
+                        sc.nextLine();
+                    }
+                   int modalidadInt = 0;
+                    while (modalidadInt != 1 && modalidadInt !=2){
+                        // solicito modalidad
+                        System.out.println("\n1. Regular\n2. Experto\nEscoje la modalidad: ");
+                        modalidadInt = sc.nextInt();
+                        sc.nextLine();
+                    }
+                    String modalidad = "";
+                    switch (modalidadInt){
+                        case 1-> modalidad = "Regular";
+                        case 2-> modalidad = "Experto";
+                    }
+
                     break;
                 // opción reglas
-                case 1:
+                case 2:
                     int reglasOpc = 0;
                     while(reglasOpc!=2) {
-                        Object[] botonesReglas = {"Regular", "Experto", "Salir"};
-                        reglasOpc = JOptionPane.showOptionDialog(null,
-                                "Selecciona para ver las reglas según la modalidad",
-                                "Reglas",
-                                JOptionPane.DEFAULT_OPTION,
-                                JOptionPane.QUESTION_MESSAGE,
-                                null,
-                                botonesReglas,
-                                botonesReglas[0]);
+                        System.out.println("\n1. Reglas modalidad Regular\n2. Reglas modalidad Experto\n3. Salir\nIngresa una opción: ");
+                        reglasOpc = sc.nextInt();
+                        sc.nextLine();
                         switch (reglasOpc) {
                             // ver reglas de modalidad regular
-                            case 0:
-                                JOptionPane.showMessageDialog(null,
-                                        "Recibe un conjunto de 10 letras y forma todas las palabras válidas" +
+                            case 1:
+                                System.out.println("\nReglas Modalidad Regular:\nRecibe un conjunto de 10 letras y forma todas las palabras válidas" +
                                                 " posibles con estas para poder puntuar.\n" +
                                         "Al formar una palabra válida, recibe 5 puntos por c/d vocal y 3 por c/d consonante.\n" +
                                         "Si el jugador no forma una palabra válida, entonces pierde 5 puntos.\n" +
                                                 "No podrás repetir palabras en una ronda.\nUna ronda termina si todos los jugadores" +
                                                 " acuerdan no poder escribir más palabras.\nAl pasar 3 rondas el ganador será aquel" +
-                                                " que haya acumulado una mayor cantidad de puntos",
-                                        "Modalidad Regular",
-                                        JOptionPane.INFORMATION_MESSAGE);
+                                                " que haya acumulado una mayor cantidad de puntos");
                                 break;
                             // ver reglas de modalidad experto
-                            case 1:
-                                JOptionPane.showMessageDialog(null,
-                                        "Recibe un conjunto de 10 letras y forma todas las palabras válidas" +
+                            case 2:
+                                System.out.println("\nReglas Modalidad Experto:\nRecibe un conjunto de 10 letras y forma todas las palabras válidas" +
                                                 " posibles con estas para poder puntuar.\n" +
                                                 "Al formar una palabra válida, recibe 5 puntos por c/d vocal y 3 por c/d consonante.\n" +
                                                 "Si el jugador no forma una palabra válida, entonces pierde 5 puntos.\n" +
@@ -78,25 +63,20 @@ public class Main {
                                                 " acuerdan no poder escribir más palabras.\nAl pasar 3 rondas el ganador será aquel" +
                                                 " que haya acumulado una mayor cantidad de puntos\n" +
                                         "Regla experto: Al inicio de cada ronda se generará una letra de manera aleatoria" +
-                                                "la cual no podrá ser utilizada para formar palabras.",
-                                        "Modalidad Experto",
-                                        JOptionPane.INFORMATION_MESSAGE);
+                                                "la cual no podrá ser utilizada para formar palabras.");
                                 break;
                             // salir de las reglas
-                            case 2:
+                            case 3:
                                 break;
                         }
                     }
                     break;
                 // opción créditos
-                case 2:
-                JOptionPane.showMessageDialog(null,
-                        "Realizado por:\nSuffo Peimbert\nFrancisco Oceguera",
-                        "Créditos",
-                        JOptionPane.INFORMATION_MESSAGE);
+                case 3:
+                System.out.println("\nCréditos:\nRealizado por:\nSuffo Peimbert\nFrancisco Oceguera");
                     break;
                 // opción salir
-                case 3:
+                case 4:
                     break;
             }
         }
