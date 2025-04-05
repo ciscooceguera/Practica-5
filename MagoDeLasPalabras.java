fe544915734328d2eb03253923c918de430ad310
ciscooceguera <oceguera.14@hotmail.com>
2025-04-05 16:44:21 -0700

    Avances en MagoDeLasPalabras

--- MagoDeLasPalabras.java
+++ MagoDeLasPalabras.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MagoDeLasPalabras {
     // atributos
     private int turno, contadorRonda,numJugadores;
     private String modalidad;
-    private ArrayList<Palabra> palabras;
+    private HashMap<Palabra,Integer> palabras;
     private ArrayList<Jugador> jugadores;
     private ArrayList<Letra> letras;
     public MagoDeLasPalabras(int numJugadores, String modalidad){
@@ -14,7 +15,7 @@ public class MagoDeLasPalabras {
         this.modalidad = modalidad;
         this.numJugadores = numJugadores;
         // inicializo arraylists
-        palabras = new ArrayList<>();
+        palabras = new HashMap<>();
         jugadores = new ArrayList<>();
         letras = new ArrayList<>();
     }
